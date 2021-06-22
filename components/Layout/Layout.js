24aa2f0f85ff6b4d00916fab145b70a407f35ab4
s.popov <s.popov@indevlab.com>
2021-06-22 13:43:39 +0300

    start app

---
+++ components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Brightness6Rounded } from "@material-ui/icons";
+import styles from "./Layout.module.css";
+
+const Layout = ({children, title="World Ranks"}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Layout
